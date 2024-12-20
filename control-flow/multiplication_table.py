709837757e99c03045c42d588528d34337ef4680
Dela-Dek1 <rickus247@gmail.com>
2024-12-20 06:31:09 -0800

    Add multiplication table to script

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 #!/bin/bash
-number = int(input("Enter a number to see its multiplication table: "))
+number = int(input("Enter a number and see its multiplication table: "))
 print(f"Multiplication Table for {number}:")
 for num in range(1, 11):
     product = number * num
